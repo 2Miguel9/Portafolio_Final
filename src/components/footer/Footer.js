72249be1ef3601f4e9f1d48dfe9d96e67f362962
2Miguel9 <108356947+2Miguel9@users.noreply.github.com>
2022-10-03 16:36:48 -0500

    hice ajustes, añadi la parte de contacto en el footer y falta ponerle los estilos

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -9,14 +9,18 @@ const Footer = () => {
         <p>Based in Your City</p>
       </div>
       <div className="footer-contact">
-        <h3>Contact me</h3>
-        <p>And let's get down to work</p>
-        <form>
-          
+        <div className="container-form">
+        <form action="#" autoComplete="off">
+          <input type="text" name="nombre" placeholder="Ingresa tu nombre"></input>
+          <input type="email" name="email" placeholder="Ingresa tu correo" class="campo"></input>
+          <textarea name="mensaje"placeholder="Ingresa tu mensaje"></textarea>
+          <input type="submit" name="enviar"
+          value="enviar mensaje"class="btn-enviar"></input>
         </form>
+       </div>
       </div>
       <div className="footer-sns">
-        <div className="design-by">Design By Your Name</div>
+        <div className="design-by"></div>
         <div className="sns-links">
           <a href="https://linkedin.com" target="_blank" rel="noreferrer">
             <i className="fab fa-linkedin linkedin"></i>
